18117c3dd0ab7882df5292ce7ab8b7bc091b2d60
seyun31 <2ne1jenna@naver.com>
2025-09-02 19:50:45 +0900

    feat(ui) : 사용자 정보 페이지 레이아웃 개선 (#2)

--- src/app/(auth)/member/signup/page.tsx
+++ src/app/(auth)/member/signup/page.tsx
@@ -1,19 +1,32 @@
+import Image from 'next/image';
+
 export default function Signup() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen">
-      {/* 사용자 정보 박스 */}
-      <div className="relative w-[30.5vw] h-[57.6vh] border-4 border-primary-90 rounded-[32px] flex flex-col items-center justify-center">
-        {/* 박스 배경 오버레이 */}
-        <div className="absolute inset-0 rounded-[32px] bg-primary-20 opacity-50 pointer-events-none" />
-      </div>
+    <div className="flex min-h-screen items-center justify-center">
+      <div className="flex flex-col items-center gap-4">
+        {/* 사용자 정보 박스 */}
+        <div className="relative w-[30.5vw] h-[57.6vh] border-4 border-primary-90 rounded-[32px] flex items-center justify-center">
+          <div className="absolute inset-0 rounded-[32px] bg-primary-20 opacity-50 pointer-events-none" />
+        </div>
 
-      {/* 시작하기 버튼 */}
-      <div className="relative">
-        {/* 버튼 배경 오버레이 */}
-        <div className="absolute inset-0 rounded-[16px] bg-primary-20 opacity-50 pointer-events-none" />
-        <button className="relative z-10 w-[30.5vw] py-4 rounded-[16px] border-2 border-primary-90 font-semibold">
-          넥스트 커리어 시작하기
-        </button>
+        {/* 로고 이미지 */}
+        <div className="absolute top-[11%] left-1/2 transform -translate-x-1/2 z-10">
+          <Image
+            src="/assets/logos/name-logo.svg"
+            alt="nextcareer 메인 로고"
+            width={0}
+            height={0}
+            className="w-[9.7vw] h-auto"
+          />
+        </div>
+
+        {/* 시작하기 버튼 */}
+        <div className="relative">
+          <div className="absolute inset-0 rounded-[16px] bg-primary-20 opacity-50 pointer-events-none" />
+          <button className="relative z-10 w-[30.5vw] h-[11.1vh] rounded-[24px] border-4 border-primary-90 text-title-medium">
+            넥스트 커리어 시작하기
+          </button>
+        </div>
       </div>
     </div>
   );
