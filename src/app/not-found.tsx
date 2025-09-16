97ff457e9a2f3d7cba60de39d796589c307c328c
seyun31 <2ne1jenna@naver.com>
2025-09-17 05:05:21 +0900

    refactor: 404페이지 수정

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,9 +1,21 @@
+import Image from 'next/image';
+
 export default function NotFound() {
   return (
-    <div className="flex items-center justify-center min-h-screen">
-      <div className="text-center">
-        <h2 className="text-2xl font-bold mb-4">페이지를 찾을 수 없습니다</h2>
-        <p className="text-gray-600">요청하신 페이지가 존재하지 않습니다.</p>
+    <div className="min-h-screen flex items-center justify-center bg-white">
+      <div className="flex flex-col items-center gap-4">
+        <Image
+          src="/assets/logos/bad-gate-star.svg"
+          alt="꿈별이 error페이지 이미지"
+          width={375}
+          height={316}
+          className="max-w-full h-auto"
+        />
+        <div className="text-center">
+          <h1 className="text-2xl font-bold text-gray-50 mb-2">
+            조건에 맞는 정보가 없습니다.
+          </h1>
+        </div>
       </div>
     </div>
   );
